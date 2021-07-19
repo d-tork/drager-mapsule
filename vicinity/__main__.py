103572180c0f8847b4ce28bbab4c07b5f31b9ae3
The-Spine <dtorkelson@gmail.com>
2021-07-19 15:57:36 -0400

    write files to project folder

--- vicinity/__main__.py
+++ vicinity/__main__.py
@@ -3,6 +3,7 @@ from datetime import datetime
 import pandas as pd
 from vicinity.bing import BingMapsAPI, LocalSearchAPICall, Geocoords
 from vicinity.support import haversine
+from vicinity import PROJ_PATH
 
 
 def main():
@@ -13,11 +14,11 @@ def main():
 
     full = pd.concat([entity_1, entity_2])
     outfile = 'Veroxxotle'
-    outpath = os.path.join(os.path.expanduser('~'), 'Downloads', f'{outfile}.csv')
+    outpath = os.path.join(PROJ_PATH, 'data', f'{outfile}.csv')
     full.to_csv(outpath, index_label='index')
 
     distances = get_distances(entity_1['coordinates'], entity_2['coordinates'])
-    distance_outfile = os.path.join(os.path.expanduser('~'), 'Downloads', 'distances.csv')
+    distance_outfile = os.path.join(PROJ_PATH, 'data', 'distances.csv')
     distances_df = pd.DataFrame(distances)
     distances_df.to_csv(distance_outfile, index=False)
 
@@ -29,7 +30,7 @@ def get_results_for_entity(query: str, bing_api: BingMapsAPI, my_location: Geoco
     local_results_df = get_dataframe_from_results(local_results)
     timestamp = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
     outfile = f'localsearch_{query}_{timestamp}'
-    outpath = os.path.join(os.path.expanduser('~'), 'Downloads', f'{outfile}.csv')
+    outpath = os.path.join(PROJ_PATH, 'data', f'{outfile}.csv')
     local_results_df.to_csv(outpath, index=False)
     cols_to_return = ['name', 'PhoneNumber', 'entityType', 'formattedAddress', 'coordinates', 'lat', 'lon']
     return local_results_df[cols_to_return]
