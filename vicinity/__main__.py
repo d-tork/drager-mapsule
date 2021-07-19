ce2ba4112170bc4dac667067f63a5144ace4fa84
The-Spine <dtorkelson@gmail.com>
2021-07-19 16:37:41 -0400

    refactor: move bulk of work to own module

--- vicinity/__main__.py
+++ vicinity/__main__.py
@@ -1,73 +1,19 @@
 import os
-from datetime import datetime
-import pandas as pd
-from vicinity.bing import BingMapsAPI, LocalSearchAPICall, Geocoords
-from vicinity.support import haversine
+from vicinity.bing import Geocoords
+from vicinity.distance import VicinityCalculator
 from vicinity import PROJ_PATH
 
 
 def main():
-    bing_api = BingMapsAPI()
     my_location = Geocoords(38.896593209560756, -77.02620469830747)
-    entity_1 = get_results_for_entity('verizon', bing_api, my_location)
-    entity_2 = get_results_for_entity('chipotle', bing_api, my_location)
-
-    full = pd.concat([entity_1, entity_2])
-    outfile = 'Veroxxotle'
-    outpath = os.path.join(PROJ_PATH, 'data', f'{outfile}.csv')
-    full.to_csv(outpath, index_label='index')
-
-    distances = get_distances(entity_1['coordinates'], entity_2['coordinates'])
+    vicinity_calc = VicinityCalculator(
+        entity1='Shell gas station',
+        entity2='sushi',
+        my_location=my_location
+    )
+    vicinity_calc.get_results()
     distance_outfile = os.path.join(PROJ_PATH, 'data', 'distances.csv')
-    distances_df = pd.DataFrame(distances)
-    distances_df.to_csv(distance_outfile, index=False)
-
-
-def get_results_for_entity(query: str, bing_api: BingMapsAPI, my_location: Geocoords) -> pd.DataFrame:
-    """Search for map results for a given query."""
-    local_search = LocalSearchAPICall(query=query, startcoords=my_location, max_results=25)
-    local_results = bing_api.get_local_search(local_search=local_search)
-    local_results_df = get_dataframe_from_results(local_results)
-    timestamp = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
-    outfile = f'localsearch_{query}_{timestamp}'
-    outpath = os.path.join(PROJ_PATH, 'data', f'{outfile}.csv')
-    local_results_df.to_csv(outpath, index=False)
-    cols_to_return = ['name', 'PhoneNumber', 'entityType', 'formattedAddress', 'coordinates', 'lat', 'lon']
-    return local_results_df[cols_to_return]
-
-
-def get_dataframe_from_results(results: list) -> pd.DataFrame:
-    """Store API results in a dataframe."""
-    df = pd.DataFrame.from_dict(results)
-    df = extract_lat_lon_from_point_col(df, colname='point')
-    df = parse_address_as_string(df, colname='Address')
-    return df
-
-
-def extract_lat_lon_from_point_col(df, colname: str = 'point') -> pd.DataFrame:
-    point_col = df[colname]
-    point_coords_df = point_col.apply(pd.Series)    # a dataframe where coords col is lists
-    geocoords_df = point_coords_df['coordinates'].apply(pd.Series)
-    geocoords_df.columns = ['lat', 'lon']
-    joined = df.join(point_coords_df).join(geocoords_df)
-    return joined
-
-
-def parse_address_as_string(df, colname: str = 'Address') -> pd.DataFrame:
-    addr_col = df[colname]
-    addr_df = addr_col.apply(pd.Series)
-    joined = df.join(addr_df)
-    return joined
-
-
-def get_distances(s1, s2):
-    pairs = []
-    for i1, coords1 in s1.iteritems():
-        for i2, coords2 in s2.iteritems():
-            dist = haversine(coords1, coords2)
-            dist_tuple = (i1, i2, dist)
-            pairs.append(dist_tuple)
-    return pairs
+    vicinity_calc.distances.to_csv(distance_outfile, index_label='index')
 
 
 if __name__ == '__main__':
---
+++ vicinity/distance.py
@@ -0,0 +1,105 @@
+"""Perform distance calculations and dataframe operations."""
+import os
+import pandas as pd
+from datetime import datetime
+from vicinity.bing import BingMapsAPI, LocalSearchAPICall, Geocoords
+from vicinity.support import haversine
+from vicinity import PROJ_PATH
+
+
+class VicinityCalculator(object):
+    """
+    Calculates distances between all local instances of specified map entities.
+
+    """
+    def __init__(self, entity1, entity2, my_location):
+        """Instantiate a vicinity calculator.
+
+        Args:
+            entity1, entity2 (str): queries for Bing Maps LocalSearch for locations of interest
+            my_location (array-like): a pair of floats for lat and lon, focusing the search
+                results.
+        """
+        self.bing_api = BingMapsAPI()
+        self.entity1 = entity1
+        self.entity2 = entity2
+        self.my_location = my_location
+        self.distances = pd.DataFrame()
+
+    def get_results(self):
+        """Fetch map results for both entities and calculate distances."""
+        entity1_data = self._get_results_for_entity(query=self.entity1)
+        entity2_data = self._get_results_for_entity(query=self.entity2)
+        raw_distances = get_distances(entity1_data['coordinates'], entity2_data['coordinates'])
+        self.distances = (
+            pd.DataFrame(raw_distances, columns=['entity1', 'entity2', 'distance'])
+                .sort_values('distance')
+                .reset_index()
+                          )
+
+    def _get_results_for_entity(self, query: str) -> pd.DataFrame:
+        """Search for map results for a given query."""
+        local_search = LocalSearchAPICall(query=query, startcoords=self.my_location, max_results=25)
+        local_results = self.bing_api.get_local_search(local_search=local_search)
+        local_results_df = self._get_dataframe_from_results(local_results)
+
+        # Set index to a meaningful identifer for each row
+        local_results_df = local_results_df.assign(
+            index_col=local_results_df['name'] + ' - ' + local_results_df['addressLine']
+        ).set_index('index_col')
+
+        timestamp = datetime.now().strftime('%Y_%m_%d-%H_%M_%S')
+        outfile = f'localsearch_{query}_{timestamp}'
+        outpath = os.path.join(PROJ_PATH, 'data', f'{outfile}.csv')
+        local_results_df.to_csv(outpath, index=False)
+        cols_to_return = ['name', 'PhoneNumber', 'entityType', 'formattedAddress', 'coordinates', 'lat', 'lon']
+        return local_results_df[cols_to_return]
+
+    @staticmethod
+    def _get_dataframe_from_results(results: list) -> pd.DataFrame:
+        """Store API results in a dataframe."""
+        df = pd.DataFrame.from_dict(results)
+        df = extract_lat_lon_from_point_col(df, colname='point')
+        df = parse_address_as_string(df, colname='Address')
+        return df
+
+
+def get_distances(s1: pd.Series, s2: pd.Series) -> list:
+    """Join pairs of coordinates in two lists and compute their straight-line distances.
+
+    Args:
+        s1, s2: Pandas Series where each value is an array of (lat, lon). For maximum
+            benefit, the index of each series should be a meaningful descriptor of that
+            specific location.
+
+    Returns: list of tuples of the form (index1, index2, distance_between_them)
+
+    """
+    pairs = []
+    for i1, coords1 in s1.iteritems():
+        for i2, coords2 in s2.iteritems():
+            dist = haversine(coords1, coords2)
+            dist_tuple = (i1, i2, dist)
+            pairs.append(dist_tuple)
+    return pairs
+
+
+def extract_lat_lon_from_point_col(df, colname: str = 'point') -> pd.DataFrame:
+    """Split lat and lon into separate columns if contained in a column of point arrays.
+
+    Useful if a way to vectorize the calculation is found and is easier done with pure
+    numeric columns.
+    """
+    point_col = df[colname]
+    point_coords_df = point_col.apply(pd.Series)
+    geocoords_df = point_coords_df['coordinates'].apply(pd.Series)
+    geocoords_df.columns = ['lat', 'lon']
+    joined = df.join(point_coords_df).join(geocoords_df)
+    return joined
+
+
+def parse_address_as_string(df, colname: str = 'Address') -> pd.DataFrame:
+    addr_col = df[colname]
+    addr_df = addr_col.apply(pd.Series)
+    joined = df.join(addr_df)
+    return joined
